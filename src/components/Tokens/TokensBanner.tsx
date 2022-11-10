4b2931fa4181228de62255072f71ba465679921a
Gtps.Finance <welcome@gtps.finance>
2022-11-10 20:16:08 +0000

    Update TokensBanner.tsx

--- src/components/Tokens/TokensBanner.tsx
+++ src/components/Tokens/TokensBanner.tsx
@@ -10,41 +10,37 @@ import styled, { useTheme } from 'styled-components/macro'
 import { opacify } from 'theme/utils'
 import { Z_INDEX } from 'theme/zIndex'
 
-import tokensPromoDark from '../../assets/images/tokensPromoDark.png'
-import tokensPromoLight from '../../assets/images/tokensPromoLight.png'
-
-const BackgroundColor = styled(Link)<{ show: boolean }>`
-  background-color: ${({ theme }) => (theme.darkMode ? theme.backgroundScrim : '#000000')};
+const BackgroundColor = styled(Link) <{ show: boolean }>`
+  background-color: ${({ theme }) => (theme.darkMode ? theme.backgroundScrim : '#0D0BB1')};
   border: 1px solid ${({ theme }) => theme.backgroundOutline};
   border-radius: 12px;
   bottom: 48px;
   box-shadow: ${({ theme }) => theme.deepShadow};
   display: ${({ show }) => (show ? 'block' : 'none')};
   height: 88px;
-  position: fixed;
+  position: unset;
   right: clamp(0px, 1vw, 16px);
   text-decoration: none;
-  width: 320px;
+  width: 400px;
   z-index: ${Z_INDEX.sticky};
 `
 const PopupContainer = styled.div`
   background-color: ${({ theme }) => (theme.darkMode ? theme.backgroundScrim : opacify(60, '#FDF0F8'))};
-  background-image: url(${({ theme }) => (theme.darkMode ? `${tokensPromoDark}` : `${tokensPromoLight}`)});
+  background-image:url (https://svgshare.com/s/nym);
   background-size: cover;
   background-blend-mode: overlay;
-  border-radius: 12px;
-  color: ${({ theme }) => theme.textPrimary};
+  border-radius: 20px;
+  color: #f7eded;
   display: flex;
   flex-direction: column;
   gap: 8px;
   height: 100%;
-  padding: 12px 16px 12px 20px;
-
+  padding: 20px 16px 12px 20px;
   transition: ${({
-    theme: {
-      transition: { duration, timing },
-    },
-  }) => `${duration.slow} opacity ${timing.in}`};
+  theme: {
+    transition: { duration, timing },
+  },
+}) => `${duration.slow} opacity ${timing.in}`};
 `
 const Header = styled.div`
   align-items: center;
@@ -59,7 +55,7 @@ const HeaderText = styled.span`
 
 const Description = styled.span`
   font-weight: 400;
-  font-size: 12px;
+  font-size: 40px;
   line-height: 16px;
   width: max(212px, calc(100% - 36px));
 `
@@ -76,7 +72,7 @@ export default function TokensBanner() {
         <PopupContainer>
           <Header>
             <HeaderText>
-              <Trans>Explore Top Tokens on Uniswap</Trans>
+              <Trans>www.appgtpsfinance.com</Trans>
             </HeaderText>
             <X
               size={20}
@@ -91,7 +87,7 @@ export default function TokensBanner() {
           </Header>
 
           <Description>
-            <Trans>Sort and filter assets across networks on the new Tokens page.</Trans>
+            <Trans>Gtps.Finance.</Trans>
           </Description>
         </PopupContainer>
       </TraceEvent>
